2078f5948e218ac2d3ef9139b0f2aacb749ee528
Jackson Alessandro dos Santos <jackson_a_santos@hotmail.com>
2020-04-11 21:21:50 -0300

    Inclusão

---
+++ ocb.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Created on Tue Dec  4  2018 - 00:43:55
+
+@author: Jackson Alessandro dos Santos
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+#import smbus
+#import time
+import datetime
+
+while True:
+    x=0
+    print(datetime.datetime)
