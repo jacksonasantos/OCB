f43adc23c662134c0ac334a77c60af2ab28f87e2
Jackson Alessandro dos Santos <jackson_a_santos@hotmail.com>
2023-09-14 00:02:03 -0300

    ajustes

--- ocb.py
+++ ocb.py
@@ -7,8 +7,20 @@ Created on Tue Dec  4  2018 - 00:43:55
 
 #import smbus
 #import time
+#import serial    # para usar com Arduino
+import RPi.GPIO as gpio
 import datetime
 
-while True:
+#arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+# Config das portas
+gpio.setmode(gpio.BCM)   # Contagem do PINOUT, no. GPIO (BCM/BOARD)
+gpio.setup(x, gpio.IN)
+
+for x in range(1,10):
+    gpio.output(x, gpio.HIGH) # HIGH/LOW)
+    #arduino.write(b'0')
     x=0
     print(datetime.datetime)
+    
+gpio.cleanup()
